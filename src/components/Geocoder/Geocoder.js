e70a7789429b9772b7b88b071a5d9e6bb077348a
Noah Mulfinger <nmulfinger@esri.com>
2023-03-06 16:09:15 -0800

    Switch to one file

--- src/components/Geocoder/Geocoder.js
+++ src/components/Geocoder/Geocoder.js
@@ -1,5 +1,9 @@
 import React from "react";
+import { useCallback, useEffect, useReducer } from "react";
 import Downshift from "downshift";
+import { ApiKeyManager } from "@esri/arcgis-rest-request";
+import { geocode, suggest } from "@esri/arcgis-rest-geocoding";
+import debounce from "lodash.debounce";
 import {
   Label,
   Menu,
@@ -10,9 +14,82 @@ import {
   XIcon,
   css,
 } from "./Styles";
-import { Suggest, geocodeResult } from "./Geocode";
 
-export default function Search() {
+// Create an `.env.local` file and add REACT_APP_API_KEY=<your-api-key>
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const initialState = {
+  data: undefined,
+  loading: true,
+  error: false,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_SUCCESS":
+      return {
+        data: action.payload,
+        loading: false,
+        error: false,
+      };
+    case "FETCH_ERROR":
+      return {
+        data: undefined,
+        loading: false,
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const authentication = ApiKeyManager.fromKey(API_KEY);
+
+export function geocodeResult(selectedItem) {
+  const { magicKey } = selectedItem;
+
+  geocode({ magicKey, authentication, params: { maxLocations: 1 } }).then(
+    (res) => {
+      const result = res.candidates[0];
+      alert(
+        `Address: ${result.address}\nLat/Long: [${result.location.y}, ${result.location.x}]`
+      );
+    }
+  );
+}
+
+export function Suggest({ address, children }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const debouncedCallback = useCallback(
+    debounce((value) => {
+      suggest(value, {
+        params: { location: [-116.539247, 33.825993], maxSuggestions: 5 },
+        authentication,
+      })
+        .then((res) => {
+          dispatch({ type: "FETCH_SUCCESS", payload: res.suggestions });
+        })
+        .catch((e) => {
+          dispatch({ type: "FETCH_ERROR", payload: e.message });
+          console.error(e);
+        });
+    }, 300),
+    []
+  );
+
+  useEffect(() => debouncedCallback(address), [address]);
+
+  const { data, loading, error } = state;
+
+  return children({
+    data,
+    loading,
+    error,
+  });
+}
+
+export default function Geocoder() {
   const handleStateChange = ({ selectedItem }) => {
     if (selectedItem) {
       geocodeResult(selectedItem);
